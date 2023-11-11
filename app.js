3feb09b17a0a2762af6284d22500f48380ddadf4
jdong800@naver.com <jdong800@naver.com>
2023-11-11 17:57:50 +0900

    11주차 업로드

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var booksRouter = require('./routes/books');
 var localRouter = require('./routes/locals');
+var postRouter = require('./routes/posts');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/books', booksRouter);
 app.use('/locals', localRouter);
+app.use('/posts', postRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
